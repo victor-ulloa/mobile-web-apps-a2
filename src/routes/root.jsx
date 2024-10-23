d56591104f4ea231f753c142fcc6e4378ea3be44
Victor Ulloa <alejandroekas@gmail.com>
2024-10-23 00:05:30 -0500

    firebase installed

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -11,7 +11,6 @@ export default function Root() {
   return (
     <>
       <div id="sidebar">
-        <h1>React Router Contacts</h1>
         <div>
           <form id="search-form" role="search">
             <input
@@ -30,9 +29,6 @@ export default function Root() {
               className="sr-only"
               aria-live="polite"
             ></div>
-          </form>
-          <form method="post">
-            <button type="submit">New</button>
           </form>
         </div>
         <nav>
